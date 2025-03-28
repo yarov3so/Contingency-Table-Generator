106103c602653f4f87c5d425f4e5edc4554b766f
Yaro <61938366+yarov3so@users.noreply.github.com>
2025-03-27 22:19:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.markdown("You have entered the following coordinates:")
 st.dataframe(datapts,hide_index=True)
 
 x_bins = st.number_input(
-    "Select the number of subintervals for the x-variable (recommended value: 5 ): ",
+    "Select the number of subintervals for the x-variable (recommended value: 5) : ",
     min_value=1,
     max_value=100,
     value=5,
@@ -76,7 +76,7 @@ x_bins = st.number_input(
 )
 
 y_bins = st.number_input(
-    "Select the number of subintervals for the y-variable (recommended value: 5 ):",
+    "Select the number of subintervals for the y-variable (recommended value: 5) :",
     min_value=1,
     max_value=100,
     value=5,
